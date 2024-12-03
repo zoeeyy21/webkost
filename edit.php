a0f7112533ce1b1d5738d847b4da647448908b90
zoeeyy21 <superspeeedy980@gmail.com>
2024-12-03 17:34:46 +0700

    few update

--- edit.php
+++ edit.php
@@ -18,7 +18,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - PAGE</title>
+    <title>Edit - PAGE</title>
     <link rel="stylesheet" href="css/editt.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
@@ -27,7 +27,7 @@
  <nav class="navbar" style="position: fixed;">
         <div class="navbar-container">
             <div class="logo">
-                <h2>Aku<span>Kost.</span></h2>
+                <h2>Juragan<span>Kost.</span></h2>
             </div>
             <ul class="menu-items">
                 <li><a href="admin.php#home">Home</a></li>
