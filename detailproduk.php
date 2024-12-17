ff09ed3a492be97212537c5cb781a560f07be179
zoeeyy21 <superspeeedy980@gmail.com>
2024-12-17 19:44:45 +0700

    DONE

--- detailproduk.php
+++ detailproduk.php
@@ -1,3 +1,12 @@
+<!-- Kelas SI-48-10 -->
+<!-- Kelompok 03 -->
+<!-- Anggota Kelompok -->
+<!-- 1. Geoffrey Putra (102022400215) -->
+<!-- 2. Cedric Satria Wibawa (102022400189) -->
+<!-- 3. Faris Yahya Ayyash Alfatih (102022400029) -->
+<!-- 4. Ridho Muhammad Zahran (102022400293) -->
+<!-- 5. Rizky Firman Nanda (102022400203) -->
+
 <?php 
     declare(strict_types=1);
     session_start();
